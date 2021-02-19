5ecf0ff57df757a1d0bf0ee7b60b69afeee70978
Henry Webster <hwebs@hwebs.info>
2021-02-19 17:39:09 -0500

    Work on theme object for light and dark mode

--- theme.js
+++ theme.js
@@ -1,34 +1,59 @@
 import { createMuiTheme } from "@material-ui/core/styles"
 
-const darkTheme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      '"Source Sans Pro"',
-      '"Roboto"',
-      '"Open Sans"',
-      "sans-serif",
-    ].join(","),
+const typography = {
+  fontFamily: ['"Source Sans Pro"', "sans-serif"].join(","),
+}
+
+const overrides = {
+  MuiTypography: {
+    h4: {
+      fontWeight: "900",
+    },
   },
+}
+
+const darkTheme = createMuiTheme({
+  typography: typography,
+  overrides: overrides,
   palette: {
     type: "dark",
     primary: {
-      main: "#2ee779",
+      light: "#66ffa6",
+      main: "#00e676",
+      dark: "#00b248",
     },
     secondary: {
+      light: "#8f7cff",
       main: "#504fff",
+      dark: "#0025cb",
     },
     action: {
-      hover: "#504fff88",
-      selected: "#504fffdd",
-    },
-  },
-  overrides: {
-    MuiTypography: {
-      h4: {
-        fontWeight: "900",
-      },
+      hover: "#504fffbb",
+      selected: "#504fff",
     },
   },
 })
 
-export { darkTheme }
+const lightTheme = createMuiTheme({
+  typography: typography,
+  overrides: overrides,
+  palette: {
+    type: "light",
+    primary: {
+      light: "#66ffa6",
+      main: "#00b248",
+      dark: "#00b248",
+    },
+    secondary: {
+      light: "#8f7cff",
+      main: "#504fff",
+      dark: "#0025cb",
+    },
+    action: {
+      hover: "#8f7cff88",
+      selected: "#8f7cffbb",
+    },
+  },
+})
+
+export { darkTheme, lightTheme }
