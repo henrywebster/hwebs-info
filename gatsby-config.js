df22a7a48092b095ead89de1df134977cafdc932
Henry Webster <hwebs@hwebs.info>
2021-02-18 22:59:06 -0500

    Add material-ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     "gatsby-transformer-json",
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-material-ui`,
     {
       resolve: "gatsby-source-dynamodb",
       options: {
