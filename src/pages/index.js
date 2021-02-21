aef2b52bcaa347c9f391486e6ed6a887e4755912
Henry Webster <hwebs@hwebs.info>
2021-02-21 09:42:23 -0500

    Imlpement dark mode; delete unused components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,125 @@
-import { createMuiTheme } from "@material-ui/core/styles"
 import React from "react"
-import { PersonalItem, InfoType } from "../components/PersonalItem"
-import { Box, Container, CssBaseline } from "@material-ui/core"
-import { ThemeProvider } from "@material-ui/core"
-import Sidebar from "../components/Sidebar"
-import { darkTheme, lightTheme } from "../../theme"
+
+import {
+  Drawer,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+  ListSubheader,
+  Box,
+  CssBaseline,
+  ThemeProvider,
+} from "@material-ui/core"
+import HomeIcon from "@material-ui/icons/Home"
+import EmojiPeopleIcon from "@material-ui/icons/EmojiPeople"
+import TwitterIcon from "@material-ui/icons/Twitter"
+import GitHubIcon from "@material-ui/icons/GitHub"
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports"
+import EmailIcon from "@material-ui/icons/Email"
 import "@fontsource/source-sans-pro"
 import "@fontsource/source-sans-pro/900.css"
 
-export const query = graphql`
-  query InformationQuery {
-    allInformationItem(
-      filter: { type: { eq: "InformationItem" }, hidden: { eq: false } }
-      sort: { fields: [priority] }
-    ) {
-      edges {
-        node {
-          text
-          iconType
-        }
-      }
-    }
-  }
-`
+import ControlPanelItem from "../components/ControlPanelItem"
+import computer from "../images/computer-animated-glow.gif"
+
+import { darkTheme, lightTheme } from "../../theme"
 
 export default function Home({ data }) {
+  const [theme, setTheme] = React.useState("dark")
+  // const themeMode = theme === "dark" ? darkTheme : lightTheme
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light")
+  }
+
+  const navigationData = [
+    { text: "Home", icon: <HomeIcon />, selected: true },
+    {
+      text: "About",
+      icon: <EmojiPeopleIcon />,
+      selected: false,
+    },
+  ]
+
+  const sidebarData = [
+    {
+      subtitle: "Navigation",
+      component: navigationData.map(item => (
+        <ListItem button selected={item.selected}>
+          <ListItemIcon>{item.icon}</ListItemIcon>
+          <ListItemText primary={item.text} />
+        </ListItem>
+      )),
+    },
+    {
+      subtitle: "Control Panel",
+      component: [{ text: "Dark Mode", checked: true }].map(item => (
+        <ControlPanelItem
+          text={item.text}
+          checked={item.checked}
+          onChange={themeToggler}
+        />
+      )),
+    },
+    {
+      subtitle: "Contact",
+      component: [
+        {
+          text: "Email",
+          icon: <EmailIcon />,
+          href: "mailto:hwebs@hwebs.info",
+        },
+        {
+          text: "GitHub",
+          icon: <GitHubIcon />,
+          href: "https://github.com/henrywebster",
+        },
+        {
+          text: "Twitter",
+          icon: <TwitterIcon />,
+          href: "https://twitter.com/hank29a",
+        },
+        {
+          text: "itch.io",
+          icon: <SportsEsportsIcon />,
+          href: "https://hank29a.itch.io/",
+        },
+      ].map(item => (
+        <ListItem button component="a" href={item.href} target="_blank">
+          <ListItemIcon>{item.icon}</ListItemIcon>
+          <ListItemText primary={item.text} />
+        </ListItem>
+      )),
+    },
+  ]
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
       <CssBaseline />
-      <Sidebar selected="home" />
+      <Drawer
+        open={true}
+        BackdropProps={{ invisible: true }}
+        variant="permanent"
+      >
+        <Box component="span" m={2}>
+          <img src={computer} width="200px" />
+          <Typography variant="h4" component="h1" color="primary">
+            Henry J. Webster
+          </Typography>
+        </Box>
+        <List>
+          {sidebarData.map(item => (
+            <>
+              <Divider />
+              <ListSubheader>{item.subtitle}</ListSubheader>
+              {item.component}
+            </>
+          ))}
+        </List>
+      </Drawer>
     </ThemeProvider>
   )
 }
