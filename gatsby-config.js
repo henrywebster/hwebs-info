2394097daa204c6554000f42bd83034a7319c305
Henry Webster <hwebs@hwebs.info>
2021-03-19 15:22:06 -0400

    Remove unsused dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,7 @@
 module.exports = {
   plugins: [
     "gatsby-transformer-json",
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-material-ui`,
-    `gatsby-plugin-sass`,
-    // {
-    //   resolve: "gatsby-source-dynamodb",
-    //   options: {
-    //     typeName: "InformationItem",
-    //     accessKeyId: `${process.env.DB_ACCESS_ID}`,
-    //     secretAccessKey: `${process.env.DB_ACCESS_KEY}`,
-    //     region: `${process.env.DB_REGION}`,
-    //     params: {
-    //       TableName: "hwebs-info",
-    //     },
-    //   },
-    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
