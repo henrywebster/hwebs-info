0f589893482c19b1fbf9629f3cac0fdd002b6474
Henry Webster <hwebs@hwebs.info>
2025-12-04 18:23:33 -0600

    reformat

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
-    {
+      {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           go
-	  pandoc
-	  darkhttpd
-	  jq
+          pandoc
+          darkhttpd
+          jq
         ];
       };
     };
